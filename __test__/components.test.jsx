39f389a30b6fcc30e8ce34d6d66cc57ac79f596e
0binny0 <binnyhh@protonmail.com>
2024-07-20 23:42:32 -0700

    tested <PowerToggleSwitch /> for lifted state (ON/OFF)

--- __test__/components.test.jsx
+++ __test__/components.test.jsx
@@ -1,15 +1,44 @@
 
-import {test} from '@jest/globals';
+import {test, advanceTimersByTime} from '@jest/globals';
 
-import {ColoredButton} from "../src/app/components.jsx";
+import {ColoredButton, PowerToggleSwitch} from "../src/app/components.jsx";
+import Simon from "../src/app/page.js"
 
 import {render, screen} from "@testing-library/react";
+import userEvent from '@testing-library/user-event';
 
 test.each([['red'], ['blue'], ['green'], ['yellow']])(
     'Verify that the %s button has an id and class to identify its color', (color) => {
         render(<ColoredButton color={color} />);
         const button = screen.getByRole("button", {name: `${color}_pad`});
-        expect(button).toHaveClass(`${color}_pad_color`);
+        expect(button).toHaveClass(`${color}`);
         expect(button).toHaveAttribute("id", `${color}_pad`);
     }
 );
+
+test.each(['off', 'on'])(
+    "Verify that a toggle switch can be switched to the %s position", (power) => {
+            const user = userEvent.setup();
+            render(<PowerToggleSwitch power={power} />);
+            const activePowerSwitch = screen.getByRole("button", {name: `power_${power}_button`});
+            expect(activePowerSwitch).toHaveClass("selected_output");
+            const inactivePowerSwitch = (
+                power === "off" ?
+                    screen.getByRole("button", {name: `power_on_button`}) :
+                    screen.getByRole("button", {name: `power_off_button`})
+            );
+            expect(inactivePowerSwitch).not.toHaveClass("selected_output");
+    }
+);
+
+// test(
+//     'Verify that toggling the power button to "ON" displays a sign to indicate the game is on',
+//     async () => {
+//         const user = userEvent.setup({advanceTimers: advanceTimersByTime, delay: 500})
+//         render(<Simon />);
+//         const power_on_button = screen.getByRole("button", {name: "turn_game_on"});
+//         await user.click(power_on_button);
+//         const game_display_info = screen.getByText("- -");
+//         expect(game_display_info).toBeInTheDocument();
+//     }
+// );
--- src/app/components.jsx
+++ src/app/components.jsx
@@ -1,11 +1,24 @@
 
-function ColoredButton({color, activeColor}) {
-    const cssClass = (
-        color === activeColor ?
-            `button active_${color}_pad_color` :
-            `button ${color}_pad_color`
-    );
-    return <button aria-label={`${color}_pad`} id={`${color}_pad`} className={cssClass}></button>
+function ColoredButton({color}) {
+    return <button aria-label={`${color}_pad`} id={`${color}_pad`} className={`pad_button ${color}`}></button>
 }
 
-export {ColoredButton};
+function PowerToggleSwitch({power}) {
+    const buttons = ["off", "on"].map(
+        (button) => {
+            const cssClass = power === button ? "selected_output power_button" : "power_button"
+            return <button key={`${button}_button`} id={`{${button}_button`} aria-label={`power_${button}_button`} className={cssClass}></button>
+        }
+    );
+    return <>
+        <div className="power_switch">
+            <p className="power_label">OFF</p>
+            <div className="power_toggle">
+                {buttons}
+            </div>
+            <p className="power_label">ON</p>
+        </div>
+    </>
+}
+
+export {ColoredButton, PowerToggleSwitch};
