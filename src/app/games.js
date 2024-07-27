04d68e807cfcc2036d5f983f0f7a8218305c04bd
0binny0 <binnyhh@protonmail.com>
2024-07-27 15:40:23 -0700

    added null between duplicate colors next to each other

---
+++ src/app/games.js
@@ -0,0 +1,71 @@
+
+export const games = [[
+    ["red"], ["red", null, "red"], ["red", null, "red", "yellow"], ["red", null, "red", "yellow", "green"],
+    ["red", null, "red", "yellow", "green", "red"], ["red", null, "red", "yellow", "green", "red", "blue"],
+    ["red", null, "red", "yellow", "green", "red", "blue", "yellow"],
+    ["red", null, "red", "yellow", "green", "red", "blue", "yellow", "green"],
+    ["red", null, "red", "yellow", "green", "red", "blue", "yellow", "green", "blue"],
+    ["red", null, "red", "yellow", "green", "red", "blue", "yellow", "green", "blue", null, "blue"],
+    ["red", null, "red", "yellow", "green", "red", "blue", "yellow", "green", "blue", null, "blue", "red"],
+    ["red", null, "red", "yellow", "green", "red", "blue", "yellow", "green", "blue", null, "blue", "red", "blue"],
+    ["red", null, "red", "yellow", "green", "red", "blue", "yellow", "green", "blue", null, "blue", "red", "blue", "yellow"],
+    ["red", null, "red", "yellow", "green", "red", "blue", "yellow", "green", "blue", null, "blue", "red", "blue", "yellow", "blue"],
+    ["red", null, "red", "yellow", "green", "red", "blue", "yellow", "green", "blue", null, "blue", "red", "blue", "yellow", "blue",
+    "green"], ["red", null, "red", "yellow", "green", "red", "blue", "yellow", "green", "blue", null, "blue", "red", "blue",
+    "yellow", "blue", "green", "red"]
+], [
+    ["red"], ["red", "blue"], ["red", "blue", "green"], ["red", "blue", "green", "red"], ["red", "blue", "green", "red", "blue"],
+    ["red", "blue", "green", "red", "blue", null, "blue"], ["red", "blue", "green", "red", "blue", null, "blue", "green"],
+    ["red", "blue", "green", "red", "blue", null, "blue", "green", "blue"],
+    ["red", "blue", "green", "red", "blue", null, "blue", "green", "blue", "red"],
+    ["red", "blue", "green", "red", "blue", null, "blue", "green", "blue", "red", "green"],
+    ["red", "blue", "green", "red", "blue", null, "blue", "green", "blue", "red", "green", "yellow"],
+    ["red", "blue", "green", "red", "blue", null, "blue", "green", "blue", "red", "green", "yellow", "blue"],
+    ["red", "blue", "green", "red", "blue", null, "blue", "green", "blue", "red", "green", "yellow", "blue", "green"],
+    ["red", "blue", "green", "red", "blue", null, "blue", "green", "blue", "red", "green", "yellow", "blue", "green", "yellow"],
+    ["red", "blue", "green", "red", "blue", null, "blue", "green", "blue", "red", "green", "yellow", "blue", "green", "yellow",
+    "green"], ["red", "blue", "green", "red", "blue", null, "blue", "green", "blue", "red", "green", "yellow", "blue",
+    "green", "yellow", "green", "red"]
+], [
+    ["yellow"], ["yellow", "green"], ["yellow", "green", "yellow"], ["yellow", "green", "yellow", "red"],
+    ["yellow", "green", "yellow", "red", "red"], ["yellow", "green", "yellow", "red", null, "red", "blue"],
+    ["yellow", "green", "yellow", "red", null, "red", "blue", null, "blue"],
+    ["yellow", "green", "yellow", "red", null, "red", "blue", null, "blue", "red"],
+    ["yellow", "green", "yellow", "red", null, "red", "blue", null, "blue", "red", "yellow"],
+    ["yellow", "green", "yellow", "red", null, "red", "blue", null, "blue", "red", "yellow", "blue"],
+    ["yellow", "green", "yellow", "red", null, "red", "blue", null, "blue", "red", "yellow", "blue", "green"],
+    ["yellow", "green", "yellow", "red", null, "red", "blue", null, "blue", "yellow", "blue", "green", null, "green"],
+    ["yellow", "green", "yellow", "red", null, "red", "blue", null, "blue", "yellow", "blue", "green", null, "green", "red"],
+    ["yellow", "green", "yellow", "red", null, "red", "blue", null, "blue", "yellow", "blue", "green", null, "green", "red", "yellow"],
+    ["yellow", "green", "yellow", "red", null, "red", "blue", null, "blue", "yellow", "blue", "green", null, "green", "red", "yellow", "blue"],
+    ["yellow", "green", "yellow", "red", null, "red", "blue", null, "blue", "yellow", "blue", "green", null, "green", "red", "yellow",
+    "blue", "blue"], ["yellow", "green", "yellow", "red", null, "red", "blue", null, "blue", "yellow", "blue", "green", null, "green",
+    "red", "yellow", "blue", null, "blue", "green"]
+
+], [
+    ["green"], ["green", "blue"], ["green", "blue", "green"], ["green", "blue", "green", "yellow"],
+    ["green", "blue", "green", "yellow", null, "yellow"], ["green", "blue", "green", "yellow", null, "yellow", "red"],
+    ["green", "blue", "green", "yellow", null, "yellow", "red", "blue"],
+    ["green", "blue", "green", "yellow", null, "yellow", "red", "blue", "yellow"],
+    ["green", "blue", "green", "yellow", null, "yellow", "red", "blue", "yellow", null, "yellow"],
+    ["green", "blue", "green", "yellow", null, "yellow", "red", "blue", "yellow", null, "yellow", "green"],
+    ["green", "blue", "green", "yellow", null, "yellow", "red", "blue", "yellow", null, "yellow", "green", "blue"],
+    ["green", "blue", "green", "yellow", null, "yellow", "red", "blue", "yellow", null, "yellow", "green", "blue", "blue"],
+    ["green", "blue", "green", "yellow", null, "yellow", "red", "blue", "yellow", null, "yellow", "green", "blue", "blue", "red"],
+    ["green", "blue", "green", "yellow", null, "yellow", "red", "blue", "yellow", null, "yellow", "green", "blue", "blue",
+    "red", "green"], ["green", "blue", "green", "yellow", "yellow", "red", "blue", "yellow", null, "yellow", "green", "blue",
+    "blue", "red", "green", "yellow"]
+], [
+    ["blue"], ["blue", "green"], ["blue", "green", "red"], ["blue", "green", "red", "red"],
+    ["blue", "green", "red", null, "red", "yellow"], ["blue", "green", "red", null, "red", "yellow", "red"],
+    ["blue", "green", "red", null, "red", "yellow", "red", "blue"], ["blue", "green", "red", null, "red", "yellow", "red", "blue", null, "blue"],
+    ["blue", "green", "red", null, "red", "yellow", "red", "blue", null, "blue", "yellow"],
+    ["blue", "green", "red", null, "red", "yellow", "red", "blue", null, "blue", "yellow", "green"],
+    ["blue", "green", "red", null, "red", "yellow", "red", "blue", null, "blue", "yellow", "green", "red"],
+    ["blue", "green", "red", null, "red", "yellow", "red", "blue", null, "blue", "yellow", "green", "red", "yellow"],
+    ["blue", "green", "red", null, "red", "yellow", "red", "blue", null, "blue", "yellow", "green", "red", "yellow", "blue"],
+    ["blue", "green", "red", null, "red", "yellow", "red", "blue", null, "blue", "yellow", "green", "red", "yellow", "blue", "red"],
+    ["blue", "green", "red", null, "red", "yellow", "red", "blue", null, "blue", "yellow", "green", "red", "yellow", "blue", "red", null, "red"],
+    ["blue", "green", "red", null, "red", "yellow", "red", "blue", null, "blue", "yellow", "green", "red", "yellow", "blue", "red",
+    null, "red", "green"]
+]];
